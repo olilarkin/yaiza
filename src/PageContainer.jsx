9111839d6e45b5ab140f04d386d3ecb9f95a4536
Owen Nicol <Owen Nicol>
2016-11-20 14:53:07 +0000

    Installed prismic.io

--- src/PageContainer.jsx
+++ src/PageContainer.jsx
@@ -1,15 +1,34 @@
 'use strict';
 // External modules
 import React from 'react'
+import Prismic from 'prismic.io'
 
 class PageContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.getPrismicData = this.getPrismicData.bind(this);
+  }
+
+  
+  componentWillMount() {
+    this.getPrismicData();
+  }
+  
+
+
+  getPrismicData() {
+    Prismic.api('https://yaiza.prismic.io/api').then(function (api) {
+      return api.query(''); // An empty query will return all the documents
+    }).then(function (response) {
+      console.log('Documents: ', response.results);
+    }, function (err) {
+      console.log('Something went wrong: ', err);
+    });
   }
   render() {
     return (
       <div>
-      Hello World!
+        Hello World!
       </div>
     );
   }
