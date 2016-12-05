5f3340ddebe2f0e2411e981c4d1fc49cd7c9415c
Owen Nicol <owennicol@gmail.com>
2016-12-05 20:35:17 +0000

    Fix menu button

--- components/Header/NavIcon.js
+++ components/Header/NavIcon.js
@@ -3,12 +3,13 @@ import React from 'react'
 import classNames from 'classnames'
 
 const NavIcon = (props) => {
+    console.log('propsd', props);
   const menuClasses = classNames({
     cross: true,
     open: props.menuIsOpen
   })
   return (
-    <div className="toggle" id="navMenuToggleButton" onClick={props.handleToggleMenu}>
+    <div className="toggle" id="navMenuToggleButton" onClick={props.toggleMenu}>
       <a href="#" className={menuClasses}><span></span></a>
     </div>
   );
