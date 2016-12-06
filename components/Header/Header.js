b2f2c325fc6dc2ab83d1265d99128601d7e43ada
Owen Nicol <owennicol@gmail.com>
2016-12-06 20:41:13 +0000

    Navigation (static)

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,21 +2,34 @@
 import React from 'react'
 import SVGLogo from '../SVG/SVGLogo'
 import NavIcon from './NavIcon'
+import Nav from './Nav'
 import { Link } from 'react-router'
+import classNames from 'classnames'
 
-const Header = (props) => (
-  <div className="header">
-    <div className="row">
-      <div className="col-sm-2"><Link to="/"><SVGLogo width={33} height={34} className="logo" /></Link></div>
-      <div className="col-sm-8"><p className="strapline">Design, Illustration &amp; Art Direction</p></div>
-      <div className="col-sm-2">
-        <div className="menu-container" onClick={props.toggleMenu}>
-          <div className="menu-text">Menu</div>
-          <NavIcon {...props} />
+const Header = (props) => {
+  const menuContainerClasses = classNames({
+    'menu-container': true,
+    active: props.menuIsOpen
+  });
+  const logoClasses = classNames({
+    logo: true,
+    active: props.menuIsOpen
+  });
+  return (
+    <div className="header">
+      <div className="row">
+        <div className="col-sm-2"><Link to="/"><SVGLogo width={33} height={34} className={logoClasses} /></Link></div>
+        <div className="col-sm-8"><p className="strapline">Design, Illustration &amp; Art Direction</p></div>
+        <div className="col-sm-2">
+          <div className={menuContainerClasses} onClick={props.toggleMenu}>
+            <div className="menu-text">Menu</div>
+            <NavIcon {...props} />
+          </div>
         </div>
       </div>
+      <Nav menuIsOpen={props.menuIsOpen} />
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
---
+++ components/Header/Nav.js
@@ -0,0 +1,31 @@
+// React
+import React from 'react'
+import SVGLogo from '../SVG/SVGLogo'
+import NavIcon from './NavIcon'
+import NavLink  from '../NavLink'
+import classNames from 'classnames'
+
+const Nav = (props) => {
+  const navClassNames = classNames({
+    'nav-container': true,
+    'active': props.menuIsOpen
+  })
+  return (
+  <div className={navClassNames}>
+    <ul className="nav">
+      <li><NavLink to="/about" />My Story</li>
+      <li><NavLink to="" />Case studies</li>
+      <li><NavLink to="" />Maserati</li>
+      <li><NavLink to="" />Ferrari</li>
+      <li><NavLink to="" />Nexio</li>
+      <li><NavLink to="" />Eurotunnel</li>
+      <li><NavLink to="" />Rako</li>
+      <li><NavLink to="" />Link Medical</li>
+      <li><NavLink to="" />Oxford Baroque</li>
+      <li><NavLink to="" />Transpor For London</li>
+    </ul>
+  </div>
+);
+}
+
+export default Nav;
