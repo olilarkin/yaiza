745223e62e4010e1a99eda702642e1d6053b9691
Owen Nicol <owennicol@gmail.com>
2016-12-08 12:09:15 +0000

    Homepage slider tweaks

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import SVGLogo from '../SVG/SVGLogo'
 import NavIcon from './NavIcon'
-import NavLink  from '../NavLink'
+import NavLink from '../NavLink'
 import classNames from 'classnames'
 
 const Nav = (props) => {
@@ -11,21 +11,21 @@ const Nav = (props) => {
     'active': props.menuIsOpen
   })
   return (
-  <div className={navClassNames}>
-    <ul className="nav">
-      <li><NavLink to="/about" />My Story</li>
-      <li><NavLink to="" />Case studies</li>
-      <li><NavLink to="" />Maserati</li>
-      <li><NavLink to="" />Ferrari</li>
-      <li><NavLink to="" />Nexio</li>
-      <li><NavLink to="" />Eurotunnel</li>
-      <li><NavLink to="" />Rako</li>
-      <li><NavLink to="" />Link Medical</li>
-      <li><NavLink to="" />Oxford Baroque</li>
-      <li><NavLink to="" />Transpor For London</li>
-    </ul>
-  </div>
-);
+    <div className={navClassNames}>
+      <ul className="nav">
+        <li><NavLink to="/about">My Story</NavLink></li>
+        <li><NavLink to="" />Case studies</li>
+        <li><NavLink to="" />Maserati</li>
+        <li><NavLink to="" />Ferrari</li>
+        <li><NavLink to="" />Nexio</li>
+        <li><NavLink to="" />Eurotunnel</li>
+        <li><NavLink to="" />Rako</li>
+        <li><NavLink to="" />Link Medical</li>
+        <li><NavLink to="" />Oxford Baroque</li>
+        <li><NavLink to="" />Transpor For London</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Nav;
