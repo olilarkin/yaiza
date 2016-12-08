e0680f4a402591ad55af2236f6799de41820831e
Owen Nicol <owennicol@gmail.com>
2016-12-08 12:04:36 +0000

    Header and nav styles

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,20 +7,16 @@ import { Link } from 'react-router'
 import classNames from 'classnames'
 
 const Header = (props) => {
-  const menuContainerClasses = classNames({
-    'menu-container': true,
-    active: props.menuIsOpen
-  });
-  const logoClasses = classNames({
-    logo: true,
+  const headerClasses = classNames({
+    header: true,
     active: props.menuIsOpen
   });
   return (
-    <div className="header">
+    <div className={headerClasses}>
       <div className="row">
-        <Link to="/" className="logo-link"><SVGLogo width={33} height={34} className={logoClasses} /></Link>
-        <div className="hidden-xs col-sm-12"><p className="strapline">Design, Illustration &amp; Art Direction</p></div>
-        <div className={menuContainerClasses} onClick={props.toggleMenu}>
+        <Link to="/" className="logo-link"><SVGLogo width={33} height={34} className="logo" /></Link>
+        <div className="hidden-xs strapline"><p>Design, Illustration &amp; Art Direction</p></div>
+        <div className="menu-button-container" onClick={props.toggleMenu}>
           <div className="menu-text">Menu</div>
           <NavIcon {...props} />
         </div>
