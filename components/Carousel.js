42d1273f17aeaa4b64565e25f74b1b043aa5e468
Owen Nicol <owennicol@gmail.com>
2016-12-09 10:43:43 +0000

    Homepage slider styles and navigation tweaks

--- components/Carousel.js
+++ components/Carousel.js
@@ -36,18 +36,24 @@ export default class extends React.Component {
           disableImagesLoaded={false} // default false 
           >
           {this.props.homepageContent.slice()
-            .sort(function(a, b){
-              return a.fragments["casestudy.homepage-slide-order"] 
-              && b.fragments["casestudy.homepage-slide-order"] 
-              && a.fragments["casestudy.homepage-slide-order"].value - b.fragments["casestudy.homepage-slide-order"].value})
+            .sort(function (a, b) {
+              return a.fragments["casestudy.homepage-slide-order"]
+                && b.fragments["casestudy.homepage-slide-order"]
+                && a.fragments["casestudy.homepage-slide-order"].value - b.fragments["casestudy.homepage-slide-order"].value
+            })
             .map((content, key) => {
               return (
                 <div className="carousel-cell" key={key}>
-                  <div className="carousel-cell__content" style={{ backgroundImage: `url(${content.fragments["casestudy.homepage-slider-image"].url})` }} >
-                    <div className="carousel-cell__text">
-                      <h2>{content.fragments["casestudy.homepage-slide-heading"].value}</h2>
-                      <p>{content.fragments["casestudy.homepage-slide-sub-heading"].value}</p>
-                    </div>
+                  <div
+                    className="carousel-cell__content"
+                    style={{ backgroundImage: `url(${content.fragments["casestudy.homepage-slider-image"].url})` }}
+                    >
+                  </div>
+                  <div className="carousel-cell__text">
+                    <h2
+                      data-subtext={content.fragments["casestudy.homepage-slide-sub-heading"].value}>
+                      {content.fragments["casestudy.homepage-slide-heading"].value}
+                    </h2>
                   </div>
                 </div>
               )
