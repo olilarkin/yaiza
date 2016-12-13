20da2910165d941020843019dde6178a0a9fd404
Owen Nicol <owennicol@gmail.com>
2016-12-13 21:27:03 +0000

    Video player

--- components/ProjectContainer.js
+++ components/ProjectContainer.js
@@ -1,6 +1,31 @@
 // External modules
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import SVGPlayIcon from '../components/SVG/SVGPlayIcon';
+const Youtube = require('react-youtube').default;
+
+const VideoPlayer = ({videoID}) => {
+  const opts = {
+    width:'100%',
+    playerVars: {
+      autoplay: false
+    }
+  };
+
+  const videoURL = `https://www.youtube-nocookie.com/embed/${videoID}?rel=0&controls=0&showinfo=0`;
+  return (
+    <div className="video-container" style={{ backgroundImage: 'url(/assets/homepage/homepage-maserati.jpg)' }}>
+      <div className="video-overlay"></div>
+      <div className="video-icon-container">
+        <SVGPlayIcon width={103} height={105} className="video-icon" />
+      </div>
+      {/*<iframe width="1280" height="720" src={videoURL} frameborder="0" allowFullScreen></iframe>*/}
+      <Youtube
+        videoId={videoID}
+        opts={opts}
+        />
+    </div>)
+};
 
 class ProjectContainer extends React.Component {
   constructor(props) {
@@ -11,28 +36,24 @@ class ProjectContainer extends React.Component {
     const content = this.props.projects && this.props.projects
       .filter(doc => doc.uid === this.props.params.id)
       .map(doc => {
-      // map through each slice and output into array
-      for (let slice of doc.getSliceZone('casestudy.contentArea').slices) {
-        slicesArray.push(slice);
-      };
-    });
+        // map through each slice and output into array
+        for (let slice of doc.getSliceZone('casestudy.contentArea').slices) {
+          slicesArray.push(slice);
+        };
+      });
 
     const pageContentOutput = slicesArray.length
       ? slicesArray.map((slice, index) => {
         console.log('slice', slice);
-        let html;
-        switch(slice.sliceType){
+        switch (slice.sliceType) {
           case 'header':
-            html = `<h1>${slice.value.blocks["0"].text}</h1>`;
-          break;
+            return (<div key={index} dangerouslySetInnerHTML={{ __html: `<h1>${slice.value.blocks["0"].text}</h1>` }} />);
           case 'content':
-            html = `<p>${slice.value.blocks["0"].text}</p>`;
-          break;
+            return (<div key={index} dangerouslySetInnerHTML={{ __html: `<p>${slice.value.blocks["0"].text}</p>` }} />);
           case 'video':
-            html = `<iframe width="1280" height="720" src="https://www.youtube-nocookie.com/embed/${slice.value.value}?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>`;
-          break;
+            return (<div key={index}><VideoPlayer videoID={slice.value.value} /></div>);
+
         }
-        return (<div key={index} dangerouslySetInnerHTML={{ __html: html }} />);
       })
       : null;
 
