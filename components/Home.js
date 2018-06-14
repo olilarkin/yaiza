55ea45de1344340dd8f24edb6730282a1fe6ab11
Owen Nicol <owennicol@users.noreply.github.com>
2018-06-14 05:51:37 +0100

    Comment out splash

--- components/Home.js
+++ components/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 import classnames from 'classnames';
 import Carousel from './Carousel';
-import Splash from './Splash';
+{/*import Splash from './Splash';*/}
 
 const Home = (props) => {
 
   return (
     <div id="home">
-      <Splash {...props} />
+    {/*<Splash {...props} />*/}
       {props.hasLoaded && 
         <Carousel homepageContent={props.homepageContent} homepageSlide={props.homepageSlide} setHomepageSlide={props.setHomepageSlide} />
       }
