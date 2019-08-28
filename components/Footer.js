17cd66b4b55da77e39df059b8c0c983af0566a37
Oli Larkin <olilarkin@googlemail.com>
2019-08-28 21:02:35 +0100

    add social icons to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import SVGFBLogo from './SVG/SVGFBLogo'
+import SVGInstaLogo from './SVG/SVGInstaLogo'
+import SVGLinkedInLogo from './SVG/SVGLinkedInLogo'
 
 const year = (new Date()).getFullYear()
 
 const Footer = () => (
   <div id="footer" className="container">
+      <a href="https://www.facebook.com/YaizaGraphics/"><SVGFBLogo /></a>
+      <a href="https://www.instagram.com/yaizagraphics/"><SVGInstaLogo /></a>
+      <a href="https://www.linkedin.com/in/yaiza-gardner-423b062a/"><SVGLinkedInLogo /></a>
       <p>&copy;{year} Yaiza Gardner | All Rights Reserved</p>
   </div>
 );
---
+++ components/SVG/SVGLinkedInLogo.js
@@ -0,0 +1,13 @@
+// React
+import React from 'react'
+import SVG from './SVG'
+
+const SVGLinkedInLogo = (props) => (
+  <SVG id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/SVG" viewBox="0 0 20 20"><title>linked_black</title><path d="M18.52,0h-17A1.45,1.45,0,0,0,0,1.4V18.52A1.46,1.46,0,0,0,1.47,20H18.52A1.46,1.46,0,0,0,20,18.52V1.4A1.46,1.46,0,0,0,18.52,0ZM5.93,17H3V7.46h3ZM4.45,6.15A1.72,1.72,0,1,1,6.17,4.43,1.72,1.72,0,0,1,4.45,6.15ZM17,17h-3V12.36c0-1.11,0-2.53-1.54-2.53s-1.78,1.2-1.78,2.45V17h-3V7.46h2.85v1.3h0a3.1,3.1,0,0,1,2.8-1.54c3,0,3.56,2,3.56,4.55Z"/></SVG>
+);
+
+SVGLinkedInLogo.propTypes = SVG.propTypes;
+
+export default SVGLinkedInLogo;
+
+
